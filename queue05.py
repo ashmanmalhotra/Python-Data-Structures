cba26e8c3a4b98b7a1011794db19a4c46684d493
ashmanmalhotra <ashmanmalhotra@gmail.com>
2022-12-02 07:46:58 +0530

    Uploaded Assignments on Queue

---
+++ queue05.py
@@ -0,0 +1,2 @@
+# Exercise - Complete the Program with List of options to operate on
+# Take the solution provided in queue04.py Program - and enhance it.
---
+++ queue06.py
@@ -0,0 +1,6 @@
+# Consider the solution provide queue04.py File and extend it
+# The Class should be capable of Creating Double Ended Queue
+#   1. At the time of Object Creation - Accept Argument defining whether it is:
+#           a: L -> R Queue
+#           b: R -> L Queue
+#   2. Based on the Case 1 - You will implement the Queue Operations.
