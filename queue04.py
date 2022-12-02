6bcb1e55519de0772a97b2844fc2bb40faa56f2e
ashmanmalhotra <ashmanmalhotra@gmail.com>
2022-12-02 07:30:55 +0530

    Added new L->R Queue Example

---
+++ queue04.py
@@ -0,0 +1,45 @@
+class QueueLR:
+    def __init__(self):
+        self.queue = []
+
+    def isEmpty(self):
+        return len(self.queue)==0
+
+    def head(self):
+        if not self.isEmpty():
+            return self.queue[-1]    # Head Element at extreme Right Direction
+        return None
+
+    def tail(self):
+        if not self.isEmpty():
+            return self.queue[0]    # Head Element at extreme Right Direction
+        return None
+
+    def enqueue(self, item):
+        self.queue.insert(0, item)
+
+    def dequeue(self):
+        if not self.isEmpty():
+            return self.queue.pop()
+        return None
+
+
+objqueue = QueueLR()
+# Testing Whether the Queue is Empty
+if objqueue.isEmpty():
+    print("The Queue is Empty")
+else:
+    print("The Queue is not Empty.")
+
+objqueue.enqueue(10)
+objqueue.enqueue(11)
+objqueue.enqueue(12)
+
+print(objqueue.queue)
+
+objqueue.dequeue()
+objqueue.dequeue()
+objqueue.dequeue()
+objqueue.dequeue()
+
+print(objqueue.queue)
